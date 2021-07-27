5ce9098c4c8fcd52df120e254cbeabcd922e0883
Michael De Luca <michael.deluca@circle-free.com>
2021-07-27 04:46:01 -0400

    chore: Fix modules, tests, and CI (SC-3063)

--- contracts/PremiumCalc.sol
+++ contracts/PremiumCalc.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.6.11;
 
-import { SafeMath } from "../../../../lib/openzeppelin-contracts/contracts/math/SafeMath.sol";
-
-import { ILoan } from "../../loan/contracts/interfaces/ILoan.sol";
+import { SafeMath } from "../modules/openzeppelin-contracts/contracts/math/SafeMath.sol";
 
 import { IPremiumCalc } from "./interfaces/IPremiumCalc.sol";
+
+interface ILoanWithPrincipalOwed {
+
+    function principalOwed() external view returns (uint256);
+
+}
 
 /// @title PremiumCalc calculates premium fees on Loans.
 contract PremiumCalc is IPremiumCalc {
@@ -22,7 +26,7 @@ contract PremiumCalc is IPremiumCalc {
     }
 
     function getPremiumPayment(address _loan) external override view returns (uint256 total, uint256 principalOwed, uint256 interest) {
-        principalOwed = ILoan(_loan).principalOwed();
+        principalOwed = ILoanWithPrincipalOwed(_loan).principalOwed();
         interest      = principalOwed.mul(premiumFee).div(10_000);
         total         = interest.add(principalOwed);
     }
--- contracts/test/PremiumCalc.t.sol
+++ contracts/test/PremiumCalc.t.sol
@@ -2,85 +2,75 @@
 pragma solidity 0.6.11;
 pragma experimental ABIEncoderV2;
 
-import { IERC20 } from "../../../../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import { DSTest } from "../../modules/ds-test/src/test.sol";
 
-import { TestUtil } from "../../../../test/TestUtil.sol";
+import { PremiumCalc } from "../PremiumCalc.sol";
 
-contract PremiumCalcTest is TestUtil {
+contract LoanWithPrincipalOwed {
 
-    function setUp() public {
-        setUpGlobals();
-        setUpPoolDelegate();
-        createBorrower();
-        setUpFactories();
-        setUpCalcs();
-        setUpTokens();
-        setUpOracles();
-        setUpBalancerPool();
-        setUpLiquidityPool();
+    uint256 public principalOwed;
+
+    constructor (uint256 _principalOwed) public {
+        principalOwed = _principalOwed;
     }
 
-    function test_premium(uint56 _loanAmt, uint256 premiumFee) public {
-        uint256 loanAmt = uint256(_loanAmt) + 10 ** 6;  // uint56(-1) = ~72b * 10 ** 6
+}
 
-        premiumFee = premiumFee % 10_000;
+contract PremiumCalcTest is DSTest {
 
-        setUpRepayments(loanAmt, 100, 1, 1);
-
-        mint("USDC",      address(bob),   loanAmt * 1000);  // Mint enough to pay interest
-        bob.approve(USDC, address(loan1), loanAmt * 1000);
-
-        uint256 beforeBal = IERC20(USDC).balanceOf(address(bob));
-
-        (uint256 total,         uint256 principal,         uint256 interest)         = loan1.getFullPayment();                         // USDC required for payment on loan
-        (uint256 total_premium, uint256 principal_premium, uint256 interest_premium) = premiumCalc.getPremiumPayment(address(loan1));  // USDC required for payment on loan
-
-        assertEq(total,         total_premium);
-        assertEq(principal, principal_premium);
-        assertEq(interest,   interest_premium);
-
-        assertEq(interest, principal * premiumCalc.premiumFee() / 10_000);
-
-        bob.makeFullPayment(address(loan1));
-
-        uint256 afterBal = IERC20(USDC).balanceOf(address(bob));
-
-        assertEq(beforeBal - afterBal, total);
+    function test_calcType() external {
+        assertTrue(new PremiumCalc(999_999).calcType() == uint8(12));
     }
 
-    function test_late_premium(uint56 _loanAmt, uint256 apr, uint16 index, uint16 numPayments, uint256 lateFee) public {
-        uint256 loanAmt = constrictToRange(_loanAmt, 10_000 * USD, 100 * 1E9 * USD, true);  // $10k to $100b, non zero
+    function test_name() external {
+        assertTrue(new PremiumCalc(999_999).name() == "FLAT");
+    }
 
-        apr     = apr     % 10_000;
-        lateFee = lateFee % 10_000;
+    function test_lateFee() external {
+        assertEq(new PremiumCalc(999_999).premiumFee(), 999_999);
+    }
 
-        setUpRepayments(loanAmt, apr, index, numPayments);
+    function test_getPremiumPayment() external {
+        PremiumCalc premiumCalc;
+        LoanWithPrincipalOwed loan;
+        uint256 total;
+        uint256 principalOwed;
+        uint256 interest;
 
-        mint("USDC",      address(bob),  loanAmt * 1000);  // Mint enough to pay interest
-        bob.approve(USDC, address(loan1), loanAmt * 1000);
+        loan                             = new LoanWithPrincipalOwed(1);
+        premiumCalc                      = new PremiumCalc(1);
+        (total, principalOwed, interest) = premiumCalc.getPremiumPayment(address(loan));
+        assertEq(total,         1);
+        assertEq(principalOwed, 1);
+        assertEq(interest,      0);
 
-        uint256 beforeBal = IERC20(USDC).balanceOf(address(bob));
+        loan                             = new LoanWithPrincipalOwed(10);
+        premiumCalc                      = new PremiumCalc(10);
+        (total, principalOwed, interest) = premiumCalc.getPremiumPayment(address(loan));
+        assertEq(total,         10);
+        assertEq(principalOwed, 10);
+        assertEq(interest,      0);
 
-        hevm.warp(loan1.nextPaymentDue() + 1);  // Payment is late
+        loan                             = new LoanWithPrincipalOwed(100);
+        premiumCalc                      = new PremiumCalc(100);
+        (total, principalOwed, interest) = premiumCalc.getPremiumPayment(address(loan));
+        assertEq(total,         101);
+        assertEq(principalOwed, 100);
+        assertEq(interest,      1);
 
-        (uint256 total,         uint256 principal,         uint256 interest)         = loan1.getFullPayment();                         // USDC required for payment on loan
-        (uint256 total_premium, uint256 principal_premium, uint256 interest_premium) = premiumCalc.getPremiumPayment(address(loan1));  // USDC required for payment on loan
+        loan                             = new LoanWithPrincipalOwed(800);
+        premiumCalc                      = new PremiumCalc(500);
+        (total, principalOwed, interest) = premiumCalc.getPremiumPayment(address(loan));
+        assertEq(total,         840);
+        assertEq(principalOwed, 800);
+        assertEq(interest,      40);
 
-        // Get late fee from regular interest payment
-        (,, uint256 interest_calc) = repaymentCalc.getNextPayment(address(loan1));
-        uint256 interest_late = lateFeeCalc.getLateFee(interest_calc);  // USDC required for payment on loan
-
-        assertEq(total,        total_premium + interest_late);
-        assertEq(principal,                principal_premium);
-        assertEq(interest,  interest_premium + interest_late);
-
-        assertEq(interest, principal * premiumCalc.premiumFee() / 10_000 + interest_late);
-
-        bob.makeFullPayment(address(loan1));
-
-        uint256 afterBal = IERC20(USDC).balanceOf(address(bob));
-
-        assertEq(beforeBal - afterBal, total);
+        loan                             = new LoanWithPrincipalOwed(0);
+        premiumCalc                      = new PremiumCalc(10_000);
+        (total, principalOwed, interest) = premiumCalc.getPremiumPayment(address(loan));
+        assertEq(total,         0);
+        assertEq(principalOwed, 0);
+        assertEq(interest,      0);
     }
 
 }
